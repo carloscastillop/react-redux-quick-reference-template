6f736f62cb2f0a206fc4bfb30302d82efde9ac39
Carlos Castillo <info@carloscastillo.cl>
2020-06-09 13:05:27 +0100

    Updates

---
+++ precache-manifest.387b30c83fffcd1e5b5b43b1a66dd329.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2cc792485c3a7995175a5ff1ce5c9c",
+    "url": "/react-redux-quick-reference-template/index.html"
+  },
+  {
+    "revision": "4ca61b54f0278a2f3384",
+    "url": "/react-redux-quick-reference-template/static/js/2.41a5d9bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-quick-reference-template/static/js/2.41a5d9bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "512cfa82856b7ad54f59",
+    "url": "/react-redux-quick-reference-template/static/js/main.b6d40b6b.chunk.js"
+  },
+  {
+    "revision": "0dcced4cb6bace204ba4",
+    "url": "/react-redux-quick-reference-template/static/js/runtime-main.8908b54f.js"
+  }
+]);
